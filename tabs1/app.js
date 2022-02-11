241798d5b4456e83fa57d966eb3ddeff20f68f19
emmy250 <emmychris491@gmail.com>
2022-02-11 17:16:51 +0200

    tab

---
+++ tabs1/app.js
@@ -0,0 +1,19 @@
+
+const Btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function(e){
+    const id = (e.target.dataset.id);
+    if(id){
+        Btns.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        articles.forEach(function(article){
+            article.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+});
